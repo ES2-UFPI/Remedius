16627513a093cbb94d57d026ed6a48c51246c9f9
Luis Enilton <110181744+LuisEnilton@users.noreply.github.com>
2024-11-13 09:33:04 -0300

    config frontend

---
+++ backend/src/main/java/com/remedius/remedius/RemediusApplication.java
@@ -0,0 +1,13 @@
+package com.remedius.remedius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemediusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemediusApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/remedius/remedius/RemediusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remedius.remedius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemediusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
